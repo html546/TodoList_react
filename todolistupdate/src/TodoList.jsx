25c6c8ab827374b23b5a885615fb41a0e1ec0f6a
chengjiang <15712859926@163.com>
2019-04-10 14:49:56 +0800

    333

--- todolistupdate/src/TodoList.jsx
+++ todolistupdate/src/TodoList.jsx
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import "./index.css";
 import { Input, Button, List } from "antd";
 import "antd/dist/antd.css";
-
-const data = [
-  "这是非常非常非常长的让人觉得不可思议的但是它语句通顺的第一条 TodoList",
-  "这是非常非常非常长的让人觉得不可思议的但是它语句通顺的第二条 TodoList",
-  "这是非常非常非常长的让人觉得不可思议的但是它语句通顺的第三条 TodoList",
-  "这是非常非常非常长的让人觉得不可思议的但是它语句通顺的第四条 TodoList"
-];
+import store from "./store";
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    console.log(store.getState());
+    this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    store.subscribe(this.handleStoreChange);
+  }
+  handleInputChange(e){
+    const action = {
+      type:'change_input_value',
+      value:e.target.value
+    }
+    store.dispatch(action);
+  }
+  handleStoreChange(){
+    this.setState(store.getState());
+  }
   render() {
     return (
       <div className="todo">
@@ -18,10 +30,22 @@ class TodoList extends Component {
           <h1>TodoList</h1>
         </div>
         <div className="todo-action">
-          <Input placeholder="todo" className="todo-input" />
+          <Input placeholder="todo" className="todo-input" value={this.state.inputValue} onChange={this.handleInputChange} />
           <Button type="primary" className="todo-submit">
             提交
           </Button>
+        </div>
+        <div className="todo-list">
+          <List
+            size="large"
+            bordered
+            dataSource={this.state.todoList}
+            renderItem={(item, index) => (
+              <List.Item>
+                {index + 1} - {item}
+              </List.Item>
+            )}
+          />
         </div>
       </div>
     );
---
+++ todolistupdate/src/store/reducer.js
@@ -0,0 +1,15 @@
+const defaultState = {
+  inputValue: "",
+  todoList: [
+    "这是非常非常非常长的让人觉得不可思议的但是它语句通顺的第一条 TodoList",
+    "这是非常非常非常长的让人觉得不可思议的但是它语句通顺的第二条 TodoList",
+    "这是非常非常非常长的让人觉得不可思议的但是它语句通顺的第三条 TodoList",
+    "这是非常非常非常长的让人觉得不可思议的但是它语句通顺的第四条 TodoList"
+  ]
+};
+
+export default (state = defaultState, action) => {
+  console.log(state);
+  console.log(action);
+  return state;
+};
